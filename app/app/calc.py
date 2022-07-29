c5e205fb8c4f8c5850844a775d69641b247181a1
Usama Muhammadi <musamazia736@gmail.com>
2022-07-29 23:28:32 +0100

    fixed linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,11 +3,12 @@ Calculator functions
 """
 
 
-def add(x,y):
+def add(x, y):
     """Add x and y retunr result."""
     return x + y
 
 
-def subtract(x,y):
+def subtract(x, y):
     """subtract x from y and return result."""
     return y - x
+    
