177c9b3b60d0b492421de3606f6688e5c7b6da74
Usama Muhammadi <musamazia736@gmail.com>
2022-07-31 19:48:25 +0100

    test & lint clear

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,4 +1,4 @@
-""" 
+"""
 Test custom Django management commands.
 """
 from unittest.mock import patch
@@ -9,6 +9,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
@@ -16,9 +17,7 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
-
         call_command('wait_for_db')
-
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
@@ -29,5 +28,5 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        self.assertEqual(patched_check.call_count, 1)
+        self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
