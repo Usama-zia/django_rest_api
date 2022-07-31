78fac075ce39376659e53471ffb04a9fe0d69f32
Usama Muhammadi <musamazia736@gmail.com>
2022-07-31 19:24:50 +0100

    Check for Errors with db

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,10 +4,10 @@ Calculator functions
 
 
 def add(x, y):
-    """Add x and y retrun result."""
+    """Add x and y return result."""
     return x + y
 
 
 def subtract(x, y):
-    """subtract x from y and return result."""
+    """Subtract x from y and return result."""
     return y - x
