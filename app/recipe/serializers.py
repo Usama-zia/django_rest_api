026d13ece3229b48eb9707212f243c36b9574773
Usama Muhammadi <musamazia736@gmail.com>
2022-08-07 16:37:39 +0100

    Image API Build

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -93,4 +93,14 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializers for uploading image to rcipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
